f5201d7ec622417b51619abfe74dce2ad77d18e3
olivercape22 <47966855+olivercape22@users.noreply.github.com>
2019-02-25 15:25:21 +0800

    Add files via upload

---
+++ MVCModels/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using MvcApplication6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Caching;
+
+namespace MvcApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Student> students;
+        public HomeController()
+        {
+            students = cache["students"] as List<Student>;
+            if (students == null)
+            {
+                students = new List<Student>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["students"] = students;
+        }
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(student);
+            }
+
+            student.Id = Guid.NewGuid().ToString();
+            students.Add(student);
+            SaveCache();
+
+            //Redirect to CustomerList page
+            return RedirectToAction("StudentList");
+        }
+
+        public ActionResult ViewStudent(string id)
+        {
+            Student student = students.FirstOrDefault(c => c.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(student);
+            }
+        }
+        public ActionResult EditStudent(string id)
+        {
+            Student student = students.FirstOrDefault(c => c.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(student);
+            }
+        }
+        [HttpPost]
+        public ActionResult EditStudent(Student student, string id)
+        {
+            var studentToEdit = students.FirstOrDefault(c => c.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                studentToEdit.StudentNumber = student.StudentNumber;
+                studentToEdit.FirstName = student.FirstName;
+                studentToEdit.LastName = student.LastName;
+                studentToEdit.MiddleName = student.MiddleName;
+                studentToEdit.Course = student.Course;
+                SaveCache();
+
+                return RedirectToAction("StudentList");
+            }
+        }
+        public ActionResult DeleteStudent(string id)
+        {
+            Student student = students.FirstOrDefault(c => c.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(student);
+            }
+        }
+        [HttpPost]
+        [ActionName("DeleteStudent")]
+        public ActionResult ConfirmDeleteStudent(string id)
+        {
+            Student student = students.FirstOrDefault(c => c.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                students.Remove(student);
+                return RedirectToAction("StudentList");
+            }
+        }
+        public ActionResult StudentList()
+        {
+            return View(students);
+        }
+    }
+}
---
+++ MVCModels/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MvcApplication6.Models
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        [Required]
+        //[StringLength(10, ErrorMessage = "Your String is too long!")]
+        [DisplayName("Student No.")]
+        public string StudentNumber { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Middle Name")]
+        public string MiddleName { get; set; }
+        [DisplayName("Course")]
+        public string Course { get; set; }
+    }
+}
